a24ddc789052e6229601b714e370d59571f54b7f
Alex <261809561+Alex-Haxe@users.noreply.github.com>
2026-07-08 21:56:28 -0700

    main

---
+++ source/Main.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.Sprite;
+import menus.TitleState;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+
+		addChild(new FlxGame(1280, 720, TitleState, 60, 60, true));
+	}
+}
